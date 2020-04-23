601373a406795776953ff4fbcdb11a7befa69501
strangelights <jbrookshire7@gmail.com>
2020-04-23 19:37:28 -0400

    Initial commit - project setup

---
+++ app/dependencies.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+return function (ContainerBuilder $containerBuilder) {
+    $containerBuilder->addDefinitions([
+        LoggerInterface::class => function (ContainerInterface $c) {
+            $settings = $c->get('settings');
+
+            $loggerSettings = $settings['logger'];
+            $logger = new Logger($loggerSettings['name']);
+
+            $processor = new UidProcessor();
+            $logger->pushProcessor($processor);
+
+            $handler = new StreamHandler($loggerSettings['path'], $loggerSettings['level']);
+            $logger->pushHandler($handler);
+
+            return $logger;
+        },
+    ]);
+};
+
+// API
+$container['api'] = function($c) {
+    $api = $c->get('settings')['api'];
+    return $api;
+};
+
+// Database
+$container['db'] = function($c) {
+    $db = $c->get('settings')['db'];
+    $pdo = new PDO($db['dsn'].':'.$db['database']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
---
+++ app/settings.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Monolog\Logger;
+
+return function (ContainerBuilder $containerBuilder) {
+    // Global Settings Object
+    $containerBuilder->addDefinitions([
+        'settings' => [
+            'displayErrorDetails' => true, // Should be set to false in production
+            'logger' => [
+                'name' => 'slim-app',
+                'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+                'level' => Logger::DEBUG,
+            ],
+            // API
+            'api' => [
+                'version' => 'v1',
+                'base_url' => 'http://localhost',
+            ],
+
+            // Database
+            'db' => [
+                'dns' => 'sqlite',
+                'database' => __dir__ . 'course_review.db',
+            ]
+
+        ],
+    ]);
+};
